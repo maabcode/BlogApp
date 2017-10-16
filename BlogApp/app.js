d5f057ff0fa50c76db72ca1d6946350c4a79bddb
afham <afh.a@hotmail.com>
2017-10-16 16:51:29 +0530

    favicon

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -23,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 
 app.use('/', routes);
 app.use('/users', users);
